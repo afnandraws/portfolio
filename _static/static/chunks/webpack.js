a1dcf0062f23c3355219816001969f5a6bc47d8e
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2023-09-14 21:01:45 +0100

    fixing styles

--- _static/static/chunks/webpack.js
+++ _static/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aece3df163fa995f"; }
+/******/ 		__webpack_require__.h = function() { return "6a6df25ed1b1f3b4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
