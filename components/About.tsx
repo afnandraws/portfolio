20ff155053e9bcaecde053347e7f5011bd6da645
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2023-09-14 17:00:25 +0100

    adding about section

--- components/About.tsx
+++ components/About.tsx
@@ -6,6 +6,38 @@ const About = () => {
 			<div className={styles.topbar}>
 				<span>About</span>
 			</div>
+			<div className={styles.content}>
+				<div>
+					<h1>My Story</h1>
+					<span>
+						Ever since I was young, I've been captivated by the endless
+						possibilities that the digital world offers. From creating digital
+						art to exploring immersive game environments and dissecting the
+						storytelling magic of movies, I've always been drawn to the creative
+						and technical aspects of these mediums.
+					</span>
+				</div>
+				<div>
+					<h1>Why Web Development</h1>
+					<span>
+						My journey into web development was a natural progression of those
+						interests. I realized that the web is a canvas where I can blend my
+						creative instincts with my technical skills. It's a space where I
+						can craft engaging and user-friendly experiences, just like the art,
+						games, and movies that have inspired me throughout my life.
+					</span>
+				</div>
+				<div>
+					<h1>My Approach</h1>
+					<span>
+						As a digital artist turned web developer paired with my knowledge of
+						UI design, I bring a unique perspective to every project I
+						undertake. I believe in the power of strong design and
+						functionality. I'm dedicated to delivering solutions that resonate
+						with users and clients alike.
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 };
