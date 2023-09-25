4dfc0d7b00fac6b4a987eac55e8977056259afc9
Afnan Khan <40701226+afnandraws@users.noreply.github.com>
2023-09-25 20:15:19 +0100

    fixing text colours

--- _static/static/chunks/webpack.js
+++ _static/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61ef38d4a3addf99"; }
+/******/ 		__webpack_require__.h = function() { return "2928c9dff63f558e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
